6f1c6410b2e9d3f95fec4517ef5b60bbc31a8906
Ryan King <kingryan14@gmail.com>
2022-06-27 16:26:11 -0500

    python version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
     extras_require={
         "local": ["pytest","pypyr","black","setuptools","sphinx"],
     },
-    python_requires='~=3.8',
+    python_requires='~=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
